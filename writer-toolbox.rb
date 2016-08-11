14cbd7b7d56171759f5875d91ebbf63a8f76b826
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-08-11 14:11:07 +0200

    Added writer-toolbox

---
+++ writer-toolbox.rb
@@ -0,0 +1,40 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class WriterToolbox < Formula
+  desc "Writer toolbox "
+  homepage "https://github.com/Infomaker/writer-toolbox"
+  url "https://github.com/Infomaker/writer-toolbox/archive/1.4.3.tar.gz"
+  version "1.4.3"
+  sha256 "649bc7f13f4ec1540317ee0400e0c7e8dc37d255f30a8a8c20809e4eab278c4a"
+
+  head "https://github.com/Infomaker/writer-toolbox.git"
+
+  depends_on "go" => :build
+  depends_on :hg => :build 
+
+  def install
+	  ENV["GOPATH"] = buildpath
+
+      system "go", "get", "github.com/aws/aws-sdk-go"
+
+ 	  system "go", "build", "-o", "writer-tool"
+
+	  bin.install "writer-tool"
+	  bash_completion.install "bash-completion/writer-tool"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test writer-toolbox`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/writer-tool"
+  end
+end
