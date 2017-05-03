aab9e2f6d6db3c4af36b2a3a94a53285d200dd96
Tobias Södergren <tobias.sodergren@infomaker.se>
2017-05-03 16:14:52 +0200

    Release 2.2.1

--- writer-toolbox.rb
+++ writer-toolbox.rb
@@ -5,8 +5,8 @@
 class WriterToolbox < Formula
   desc "Writer toolbox "
   homepage "https://github.com/Infomaker/writer-toolbox"
-  url "https://github.com/Infomaker/writer-toolbox/archive/2.2.tar.gz"
-  sha256 "c289b036bd40c78f41ddcbe03a97c1b9552950d9dcc1aa1e5901d0cd54b690f5"
+  url "https://github.com/Infomaker/writer-toolbox/archive/2.2.1.tar.gz"
+  sha256 "a24b5ef9cda7da0649935399e8d953b576cd5cf01d488c3b7968315f66af09b1"
 
   depends_on "go" => :build
 
@@ -15,7 +15,7 @@ class WriterToolbox < Formula
 
       system "go", "get", "github.com/aws/aws-sdk-go"
 
- 	  system "go", "build", "-o", "writer-tool", "-ldflags", "-X main.appVersion=2.2"
+ 	  system "go", "build", "-o", "writer-tool", "-ldflags", "-X main.appVersion=2.2.1"
 
 	  bin.install "writer-tool"
 	  bash_completion.install "bash-completion/writer-tool"
